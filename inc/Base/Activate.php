c0699ce969fb574ec864d381615843eb0f92a8ee
Fazle Bari <fazlebarisn@gmail.com>
2021-02-02 23:34:03 +0600

    Set default value when plugin active for first time

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -8,5 +8,21 @@ class Activate
 {
 	public static function activate(){
 		flush_rewrite_rules();
+
+		/*
+		** Set some default data when plugin install for first time
+		*** if we not set default data 
+		*** when we active plugin for first time and try to active any section from dashboard, 
+		*** all section will active
+		 */
+
+		 // if active any section from before, then do not change anything
+		 if( get_option( 'kbs_plugin' ) ){
+			 return;
+		 }
+
+		 // Set default vale
+		 $default = array();
+		 update_option( 'kbs_plugin' , $default );
 	}
 }
